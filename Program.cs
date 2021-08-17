55f9f0a7e68ad2875606ab5a8c4bac15c9450615
Sean W <86800108+TwoBreadTom@users.noreply.github.com>
2021-08-17 11:31:40 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
-﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MazeApp
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             var serviceCollection = new ServiceCollection(); // create new ServiceCollection object
-            serviceCollection.AddSingleton<IMazeSolver, MazeSolver>(); // add DiceGame class to service collection and include the interface
+            serviceCollection.AddSingleton<IMazeSolver, MazeSolver>(); // add MazeSolver class to service collection and include the interface
             serviceCollection.AddSingleton<IMazeCreator, MazeCreator>();
             var serviceProvider = serviceCollection.BuildServiceProvider(); // build the service provider
-            var mazeSolver = serviceProvider.GetService<IMazeSolver>(); // initialize diceGame as the DiceGame class through the IDiceGame interface in serviceProvider
+            var mazeSolver = serviceProvider.GetService<IMazeSolver>(); // initialize mazeSolver as the MazeSolver class through the IMazeSolver interface in serviceProvider
             var mazeCreator = serviceProvider.GetService<IMazeCreator>();
             var maze = mazeCreator?.Create();
             mazeSolver?.Run(maze); 
